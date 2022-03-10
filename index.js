7b4eaec6a961b8deef4cc08b72db743b52fbfcaf
unknown <kylelarsen12@gmail.com>
2022-03-09 22:32:12 -0500

    Finished index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your Code Here
+//Obtain user name
+let userName = window.prompt("Please enter your name: ");
+
+//Ask Questions
+let userScore = 0;
+for (let i = 0; i < questions.length; i++) {
+  let question = questions[i];
+  let userAnswer = window.prompt(questions[i].text);
+
+  if (userAnswer === question.correctAnswer) {
+    userScore += 10;
+  }
+}
+
+window.alert(`Your score is: $(userScore)`);
