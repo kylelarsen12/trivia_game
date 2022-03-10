5e9249cb4a997d305e35a49b35aedcb14ce11e71
unknown <kylelarsen12@gmail.com>
2022-03-09 22:46:47 -0500

    Added replay functionality

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 //Obtain user name
 let userName = window.prompt("Please enter your name: ");
+let playAgain = true;
 
-//Ask Questions
-let userScore = 0;
-for (let i = 0; i < questions.length; i++) {
-  let question = questions[i];
-  let userAnswer = window.prompt(questions[i].text);
+//game
+function playGame() {
+  let userScore = 0;
+  for (let i = 0; i < questions.length; i++) {
+    let question = questions[i];
+    let userAnswer = window.prompt(questions[i].text);
 
-  if (userAnswer === question.correctAnswer) {
-    userScore += 10;
+    if (userAnswer === question.correctAnswer) {
+      userScore += 10;
+    }
   }
+
+  window.alert(`Congratulations ${userName} your score is: ${userScore}`);
 }
 
-window.alert(`Your score is: $(userScore)`);
+while (playAgain === true) {
+  playGame();
+  let replay = window.prompt("Would you like to play again? Enter yes or no: ");
+  if (replay === "yes" || replay === "Yes") {
+    playAgain = true;
+  } else {
+    playAgain = false;
+    window.alert("Thanks for playing!");
+  }
+}
